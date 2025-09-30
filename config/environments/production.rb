f999e7596fae1e2cc74ec04e2fb4c9da0114e439
SR Wicaksono <github@lab.sandyrw.com>
2025-09-30 09:59:13 +0700

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,17 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "localhost",
+    user_name:            Rails.application.credentials.dig(:gmail, :username),
+    password:             Rails.application.credentials.dig(:gmail, :app_password),
+    authentication:       :plain,
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5
+  }
+
   Rails.application.routes.default_url_options[:host] = "talkwith.sandyrw.com"
 end
